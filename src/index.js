40aa91dd1eb7af7f06e98bf28355eb7517028dc6
FomenkoVD <fom.stud@yandex.ru>
2024-04-25 19:18:49 +0500

    test lint

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import './index.html' // чтобы следил за файлом!
 import './styles.css'
-
-btn.addEventListener("click",()=>{
-    fetch('http://localhost:3000/posts')
-    .then(response => response.json())
-    .then(data=>{
-        let newdiv = document.createElement('div')
-        let ul = document.createElement('ul')
-        data.forEach(d=>{
-            let li = document.createElement('li')
-            li.innerHTML = `ID:${d['id']}   TITLE:${d['title']}     AUTHOR:${d['author']}`
-            ul.appendChild(li)
-        })
-        newdiv.appendChild(ul)
-        document.body.appendChild(newdiv) 
-        
+console.log('alls ok')
+const btn = document.querySelector('#btn')
+btn.addEventListener('click', () => {
+  fetch('http://localhost:3000/posts')
+    .then((response) => response.json())
+    .then((data) => {
+      const newdiv = document.createElement('div')
+      const ul = document.createElement('ul')
+      data.forEach((d) => {
+        const li = document.createElement('li')
+        li.innerHTML = `ID:${d.id}   TITLE:${d.title}     AUTHOR:${d.author}`
+        ul.appendChild(li)
+      })
+      newdiv.appendChild(ul)
+      document.body.appendChild(newdiv)
     })
 })
